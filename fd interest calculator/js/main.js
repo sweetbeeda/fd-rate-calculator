39be373a53e7f98c0d5e8367616e2bc0688c7d67
sweetbeeda <yashwanth2453@gmail.com>
2025-01-13 13:46:27 +0530

    folder name were changed as calculator

--- fd interest calculator/js/main.js
+++ fd interest calculator/js/main.js
@@ -7,13 +7,18 @@
 function fdRate() {
   let months = Number(document.getElementById("input").value);
   let rate = 0;
-  if (months < 3) rate = 5.8;
-  else if (months >= 3 && months <= 6) rate = 6.5;
-  else if (months >= 7 && months <= 9) rate = 6.8;
+  if (months<3)
+     rate = 5.8;
+  else if (months>=3 && months<=6)
+     rate = 6.5;
+  else if (months>=7 && months<=9) 
+     rate = 6.8;
   else;
-  rate = 7;
+     rate = 7;
+
+
   let result = document.getElementById("result");
-  result.innerHTML = `your interest rate is ${rate} %`
+  result.innerHTML = "your interest rate is " + rate +"%"
   console.log(result);
   
 }
